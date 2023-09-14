53e2a3de142eb02811d28656ff4a2d1f38c0bb43
Elizabeth Neary <20071724@mail.wit.ie>
2023-09-14 16:51:26 +0100

    Completed Week 1 Lab

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Task from './components/Task';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Tasky</h1>
+      <Task title="Dishes" deadline="Today" desc="Clean mouldy dishes in sink"/>
+      <Task title="Laundry" deadline="Tomorrow" desc="Fold Laundry and put away"/>
+      <Task title="Tidy" deadline="Today" desc="Hoover and dust rooms"/>
     </div>
   );
 }
