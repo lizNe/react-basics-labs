b2c351b503043b7f67d64a28f1c8c331d50fcc2e
Elizabeth Neary <20071724@mail.wit.ie>
2023-09-26 14:01:59 +0100

    Adding Done button with background color change when done

--- src/App.js
+++ src/App.js
@@ -6,26 +6,36 @@ import React, { useState } from 'react';
 function App() {
   const [ taskState, setTaskState ] = useState({
     tasks: [
-      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today" },
-      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" },
-      { id: 3, title: "Tidy up", description: "Hoover and Dust",deadline: "Today" },
-      { id: 4, title: "Pets", description: "Clean pens",deadline: "Next Weekend" },
-      { id: 5, title: "Notes", description: "Write College Notes",deadline: "Now" }
+      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today", done: false },
+      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", done: false },
+      { id: 3, title: "Tidy up", description: "Hoover and Dust",deadline: "Today", done: false },
+      { id: 4, title: "Pets", description: "Clean pens",deadline: "Next Weekend" ,done: false},
+      { id: 5, title: "Notes", description: "Write College Notes",deadline: "Now",done: false }
 
     ]
   });
+
+   const doneHandler = (taskIndex) => {
+    const tasks = [...taskState.tasks];
+    tasks[taskIndex].done = !tasks[taskIndex].done;
+    setTaskState({tasks});
+    console.log(`${taskIndex} ${tasks[taskIndex].done}`);
+  }
     return (
+      
       <div className="container">
         <h1>Tasky</h1>
 
-        {taskState.tasks.map((task) => (              
+        {taskState.tasks.map((task, index) => (              
     <Task 
       title={task.title}
       description={task.description}
       deadline={task.deadline}
       key={task.id}
+      done={task.done}
+      markDone={() => doneHandler(index)}
     />
-  ))} 
+  ))}
       </div>
     );
 }
